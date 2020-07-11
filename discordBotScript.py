bb33403fc9717028c6545296d3afd8a75a018527
SnowFleix <38901046+SnowFleix@users.noreply.github.com>
2020-07-11 23:29:22 +0100

    Added a list of banned words for the on_message command

--- discordBotScript.py
+++ discordBotScript.py
@@ -1,24 +1,18 @@
-import _thread
 import discord
-from discord.utils import get
 
-
-def time_user_out(message):
-    role = get(message.author.roles, name="Timeout")
-    client.add_role(message.author, role)
+bannedWords = {"puta", "porca", "burra", "burro", "estupida", "estupido"}
 
 
 class PortugalClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
     async def on_message(self, message):
-        if message.author.name == "fleix" and "puta" in message.content:
-            _thread.start_new(time_user_out(message), ())
-        if message.author.name == "|Inês| (ijwtbhfe)" and "puta" in message.content:
-            await message.channel.send("Sim puta, veeerrrryyyy smart")
-        #print(message)
+        if any(word in message.content for word in bannedWords):
+            await message.channel.send("Don't make me simp you, if you say that shit again imma simp you bad")
     async def on_message_delete(self, message):
         if message.author.name == "|Inês| (ijwtbhfe)":
+            await message.channel.send('{0} tried to delete: {1}'.format(message.author.nick, message.content))
+        if message.author.name == "fleix":
             await message.channel.send('{0} tried to delete: {1}'.format(message.author.nick, message.content))
 
 
