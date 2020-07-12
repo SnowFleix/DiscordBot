f5a1ff291912902966f5ca3f39d5c21c242337af
SnowFleix <38901046+SnowFleix@users.noreply.github.com>
2020-07-13 00:27:28 +0100

    Updated botscript to be more suited to changes

--- discordBotScript.py
+++ discordBotScript.py
@@ -8,12 +8,11 @@ class PortugalClient(discord.Client):
         print('Logged on as {0}!'.format(self.user))
     async def on_message(self, message):
         if any(word in message.content for word in bannedWords):
-            await message.channel.send("Don't make me simp you, if you say that shit again imma simp you bad")
+            await message.channel.send("Add text here")
     async def on_message_delete(self, message):
-        if message.author.name == "|Inês| (ijwtbhfe)":
-            await message.channel.send('{0} tried to delete: {1}'.format(message.author.nick, message.content))
-        if message.author.name == "fleix":
-            await message.channel.send('{0} tried to delete: {1}'.format(message.author.nick, message.content))
+        #if message.author.name == "": # add this if you want only one person to have their deleted messages reposted
+        await message.channel.send('{0} tried to delete: {1}'.format(message.author.nick, message.content))
 
 
 client = PortugalClient()
+client.run("Your token goes here")
