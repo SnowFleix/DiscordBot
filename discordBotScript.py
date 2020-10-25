eae82372b841a0f7f8d1406b9990c5370d07ed5b
Felix England <38901046+SnowFleix@users.noreply.github.com>
2020-10-25 22:19:48 +0000

    Update discordBotScript.py

--- discordBotScript.py
+++ discordBotScript.py
@@ -11,7 +11,7 @@ class PortugalClient(discord.Client):
         elif message.content.lower() == "po caralho bot de merda":
             await message.channel.send("No u")
         elif "estupida bot" in message.content.lower():
-            await message.channel.send("I'm sorry I hurt your feelings when I called you stupid, I just thought you already knew")
+            await message.channel.send("Ok")
     async def on_message_delete(self, message):
         if message.author.name == "|Inês| (ijwtbhfe)":
             await message.channel.send('{0} tried to delete: {1}'.format(message.author.nick, message.content))
